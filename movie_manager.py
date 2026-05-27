b50b5496eaea170a8f3f836a0d69b3214100fdd8
shahedshehab <shahedshehab75@gmail.com>
2026-05-27 22:18:45 +0300

    implement add movies method

--- movie_manager.py
+++ movie_manager.py
@@ -11,8 +11,7 @@ class MovieManager():
         except FileNotFoundError:
             raise FileNotFoundError(f"{filepath} File not found!!!")
     
-    def save_movies(self,movies:list,filepath: str):
-        def save_movies(self,movies:list,filepath: str) -> None:
+    def save_movies(self,movies:list,filepath: str) -> None:
             with open(filepath, 'w', newline="", encoding='utf-8')as f:
                 writer=csv.DictWriter(f,fieldnames=['title','genre','year','rating','watched'])
                 writer.writeheader()
@@ -20,12 +19,18 @@ class MovieManager():
             print(f"movies is saved to {filepath}") 
     
     def add_movie(self,movies: list, title: str, genre: str, year: int,rating: float,watched: str)-> list:
-        pass
+        if rating<0.0 or rating>10.0:
+            raise ValueError("rating must be between 0.0-10.0")
+        if watched != "yes" and watched != "no":
+            raise ValueError("watched must be yes or no")
+        movie={'title':title,'genre':genre,"year":year,'rating':rating,'watched':watched}
+        movies.append(movie)
+        return movies
     
     def get_watched(self, movies: list)-> list:
         pass
 
 #just for pre testing
-m=MovieManager()
-print(m.load_movies("movies.csv"))
-m.save_movies([{'title':"The Running Man",'genre':'action','year':2025,'rating':6.4,'watched':"No"}],"movies.csv")
+# m=MovieManager()
+# print(m.load_movies("movies.csv"))
+# m.save_movies([{'title':"The Running Man",'genre':'action','year':2025,'rating':6.4,'watched':"no"}],"movies.csv")
